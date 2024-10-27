51d98b2d1e5e101d196c11124a4ab0c9ae50c897
Map-Geek <geetha.ramesh@dcmail.ca>
2024-10-27 14:35:41 -0700

    Add test for non string input

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,28 @@
 """
-Validates strings as palindromes.
+Python Development II Assignment 4: Palindrome Validator
+Geetha Ramesh
+
+palindrome.py
+
+This module provides an 'is_palindrome' function that determines if a given
+string is a palindrome and returns a boolean: True or False.
+
+Functions:
+    - is_palindrome(input_string: str) -> bool: Checks if the input string is a palindrome.
+
+Exceptions:
+    - Raises a ValueError for non-string inputs.
 """
+
+
+def is_palindrome(input_string):
+    """
+    Validate if the input string is a palindrome.
+
+    :param input_string: the string to be checked if it is a palindrome
+    :return: True or False
+    :raises ValueError: if input is not a string
+    """
+
+    if not isinstance(input_string, str):
+        raise ValueError("The input value must be a string")
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,3 +1,28 @@
 """
-Tests the palindrome module
+Python Development II Assignment 4: Palindrome Validator
+Geetha Ramesh
+
+test_palindrome.py
+
+Unit tests for the is_palindrome function.
+
+This module contains test cases to validate the behavior of the
+is_palindrome function, which returns a boolean (True or False)
+for a given string.
 """
+
+import pytest
+from palindrome import is_palindrome
+
+
+def test_non_string_input():
+    """
+    Test to check if is_palindrome function raises
+    a ValueError for any input that is not a string.
+    """
+    with pytest.raises(ValueError):
+        is_palindrome(7.5)
+    with pytest.raises(ValueError):
+        is_palindrome(754)
+    with pytest.raises(ValueError):
+        is_palindrome(None)
